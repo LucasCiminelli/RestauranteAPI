80e40d56044f070e5d46cf9ec92b23099a7d9b36
LucasCiminelli <lucasaciminelli@gmail.com>
2025-04-07 14:00:51 -0300

    adding tests for uploadRestaurantLogo command, and RestaurantUserClaimPrincipalFactory

---
+++ tests/Restaurants.ApplicationTests/Restaurants/Commands/UploadRestaurantLogo/UploadRestaurantLogoCommandHandlerTests.cs
@@ -0,0 +1,133 @@
+﻿using Xunit;
+using Restaurants.Application.Restaurants.Commands.UploadRestaurantLogo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Restaurants.Domain.Respositories;
+using Restaurants.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Constants;
+using FluentAssertions;
+using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.Application.Restaurants.Commands.UploadRestaurantLogo.Tests
+{
+
+    public class UploadRestaurantLogoCommandHandlerTests
+    {
+
+        private readonly Mock<ILogger<UploadRestaurantLogoCommandHandler>> _loggerMock;
+        private readonly Mock<IRestaurantRepository> _restaurantRepositoryMock;
+        private readonly Mock<IRestaurantAuthorizationService> _restaurantAuthorizationService;
+        private readonly Mock<IBlobStorageService> _blobStorageServiceMock;
+        private readonly UploadRestaurantLogoCommandHandler _handler;
+
+        public UploadRestaurantLogoCommandHandlerTests()
+        {
+            _loggerMock = new Mock<ILogger<UploadRestaurantLogoCommandHandler>>();
+            _restaurantRepositoryMock = new Mock<IRestaurantRepository>();
+            _restaurantAuthorizationService = new Mock<IRestaurantAuthorizationService>();
+            _blobStorageServiceMock = new Mock<IBlobStorageService>();
+            _handler = new UploadRestaurantLogoCommandHandler(
+                _loggerMock.Object,
+                _restaurantRepositoryMock.Object,
+                _restaurantAuthorizationService.Object,
+                _blobStorageServiceMock.Object);
+        }
+
+        [Fact()]
+        public async Task Handle_WithValidRequest_ShouldUploadLogo()
+        {
+            var restaurantId = 1;
+
+            var restaurant = new Restaurant
+            {
+                Id = restaurantId
+            };
+
+            var uploadCommand = new UploadRestaurantLogoCommand()
+            {
+                RestaurantId = restaurantId,
+
+            };
+
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync(restaurant);
+
+            _restaurantAuthorizationService.Setup(a => a.Authorize(restaurant, ResourceOperation.Update)).Returns(true);
+
+            _blobStorageServiceMock.Setup(b => b.UploadToBlobAsync(It.IsAny<Stream>(), It.IsAny<string>())).ReturnsAsync(It.IsAny<string>());
+
+
+
+            await _handler.Handle(uploadCommand, CancellationToken.None);
+
+
+            _restaurantRepositoryMock.Verify(r => r.GetByIdAsync(It.IsAny<int>()), Times.Once);
+            _restaurantAuthorizationService.Verify(r => r.Authorize(It.IsAny<Restaurant>(), ResourceOperation.Update), Times.Once);
+            _blobStorageServiceMock.Verify(b => b.UploadToBlobAsync(It.IsAny<Stream>(), It.IsAny<string>()), Times.Once);
+            _restaurantRepositoryMock.Verify(r => r.UpdateAsync(It.IsAny<Restaurant>()), Times.Once);
+
+
+        }
+
+        [Fact()]
+        public async Task Handle_WithNonExistingRestaurant_ShouldReturnNotFoundException()
+        {
+            var restaurantId = 1;
+
+            var restaurant = new Restaurant
+            {
+                Id = restaurantId
+            };
+
+            var uploadCommand = new UploadRestaurantLogoCommand()
+            {
+                RestaurantId = restaurantId,
+
+            };
+
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync((Restaurant?)null);
+
+
+            Func<Task> act = async () => await _handler.Handle(uploadCommand, CancellationToken.None);
+
+
+            await act.Should().ThrowAsync<NotFoundException>();
+
+
+        }
+
+        [Fact()]
+        public async Task Handle_WithUnauthorizedUser_ShouldReturnForbidException()
+        {
+            var restaurantId = 1;
+
+            var restaurant = new Restaurant
+            {
+                Id = restaurantId
+            };
+
+            var uploadCommand = new UploadRestaurantLogoCommand()
+            {
+                RestaurantId = restaurantId,
+
+            };
+
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync(restaurant);
+
+            _restaurantAuthorizationService.Setup(a => a.Authorize(restaurant, ResourceOperation.Update)).Returns(false);
+
+
+            Func<Task> act = async () => await _handler.Handle(uploadCommand, CancellationToken.None);
+
+
+            await act.Should().ThrowAsync<ForbidException>();
+
+
+        }
+    }
+}
---
+++ tests/Restaurants.InfrastructureTests/Authorization/RestaurantsUserClaimsPrincipalFactoryTests.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using Restaurants.Infrastructure.Authorization;
+using System;
+using System.Linq;
+using Restaurants.Domain.Entities;
+using Moq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using FluentAssertions;
+using System.Security.Claims;
+
+namespace Restaurants.Infrastructure.Authorization.Tests
+{
+    public class RestaurantsUserClaimsPrincipalFactoryTests
+    {
+        private readonly Mock<UserManager<User>> _userManagerMock;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RestaurantsUserClaimsPrincipalFactoryTests()
+        {
+            var userStoreMock = new Mock<IUserStore<User>>();
+            _userManagerMock = new Mock<UserManager<User>>(
+                userStoreMock.Object,
+                null!, null!, null!, null!, null!, null!, null!, null!
+            );
+
+            var roleStoreMock = new Mock<IRoleStore<IdentityRole>>();
+            _roleManager = new RoleManager<IdentityRole>(
+                roleStoreMock.Object,
+                new IRoleValidator<IdentityRole>[0],
+                new UpperInvariantLookupNormalizer(),
+                new IdentityErrorDescriber(),
+                null!
+            );
+        }
+
+        [Fact]
+        public async Task Handle_WhenUserHasDateOfBirthAndNationality_ShouldAddClaimsToPrincipal()
+        {
+
+            // Arrange
+
+            var options = Options.Create(new IdentityOptions());
+
+            var user = new User
+            {
+                Id = "123",
+                UserName = "testuser",
+                DateOfBirth = new DateOnly(1995, 12, 12),
+                Nationality = "Argentine"
+            };
+
+             _userManagerMock
+             .Setup(m => m.GetUserIdAsync(It.IsAny<User>()))
+             .ReturnsAsync("123");
+
+              _userManagerMock
+              .Setup(m => m.GetUserNameAsync(It.IsAny<User>()))
+              .ReturnsAsync("testuser");
+
+            var factory = new RestaurantsUserClaimsPrincipalFactory(_userManagerMock.Object, _roleManager, options);
+
+
+            // Act
+            var principal = await factory.CreateAsync(user);
+            var claims = principal.Claims.ToList();
+
+
+            // Assert
+            claims.Should().ContainSingle(c => c.Type == AppClaimTypes.Nationality && c.Value == "Argentine");
+            claims.Should().ContainSingle(c => c.Type == AppClaimTypes.DateOfBirth && c.Value == "1995-12-12");
+        }
+
+        
+
+    }
+}
