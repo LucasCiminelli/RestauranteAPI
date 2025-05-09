7424f1af3d620e0264b397a20025ce4176673113
LucasCiminelli <lucasaciminelli@gmail.com>
2025-05-09 15:31:20 -0300

    adding tests for CreateBipper Feature and UpdateBipper Feature

--- tests/Restaurants.ApplicationTests/Bippers/Commands/UpdateBipper/UpdateBipperCommandHandlerTests.cs
+++ tests/Restaurants.ApplicationTests/Bippers/Commands/UpdateBipper/UpdateBipperCommandHandlerTests.cs
@@ -16,6 +16,7 @@ using Restaurants.Application.Bippers.Dtos;
 using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using FluentAssertions;
+using Restaurants.Domain.Exceptions;
 
 namespace Restaurants.Application.Bippers.Commands.UpdateBipper.Tests
 {
@@ -102,10 +103,10 @@ namespace Restaurants.Application.Bippers.Commands.UpdateBipper.Tests
                 (cmd, b)
                 =>
                 {
-                  if (cmd.Status.HasValue)
-                    b.Status = cmd.Status.Value;
-                  if (cmd.IsReady.HasValue)
-                    b.IsReady = cmd.IsReady.Value;
+                    if (cmd.Status.HasValue)
+                        b.Status = cmd.Status.Value;
+                    if (cmd.IsReady.HasValue)
+                        b.IsReady = cmd.IsReady.Value;
                 }
             )
             .Returns(bipper);
@@ -114,7 +115,7 @@ namespace Restaurants.Application.Bippers.Commands.UpdateBipper.Tests
 
 
             //act
-            
+
             var result = await _handler.Handle(command, CancellationToken.None);
 
 
@@ -126,6 +127,294 @@ namespace Restaurants.Application.Bippers.Commands.UpdateBipper.Tests
             bipper.Id.Should().Be(bipperId);
             bipper.Status.Should().Be(BipperStatus.Notified);
             bipper.IsReady.Should().BeTrue();
+        }
+
+        [Fact()]
+        public async Task UpdateBipper_WithNullCurrentUser_ReturnsInvalidOperationException()
+        {
+
+
+            //arrange
+
+            var restaurantId = 1;
+            var restaurant = new Restaurant { Id = restaurantId };
+            var currentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+            var client = new Client { Id = 1 };
+            var bipper = new Bipper { RestaurantId = restaurantId };
+
+            var command = new UpdateBipperCommand { RestaurantId = restaurantId, ClientId = 1 };
+
+            _clientRepositoryMock.Setup(c => c.FindByIdAsync(1)).ReturnsAsync(client);
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync(restaurant);
+            _userContextMock.Setup(u => u.GetCurrentUser()).Returns((CurrentUser)null!);
+
+            _bipperAuthorizationServiceMock
+            .Setup(bas => bas.EnsureIsAuthenticated(It.Is<CurrentUser>(u => u == null)))
+            .Throws(new InvalidOperationException("User not authenticated"));
+
+
+            //act
+
+            Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+
+            //assert
+
+            await act.Should().ThrowAsync<InvalidOperationException>();
+        }
+
+        [Fact()]
+        public async Task UpdateBipper_WithNonExistingRestaurant_ReturnsNotFoundException()
+        {
+            var restaurantId = 1;
+            var restaurant = new Restaurant { Id = restaurantId };
+            var currentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+            var client = new Client { Id = 1 };
+            var bipper = new Bipper { RestaurantId = restaurantId };
+
+            var command = new UpdateBipperCommand { RestaurantId = restaurantId, ClientId = 1 };
+
+            _clientRepositoryMock.Setup(c => c.FindByIdAsync(1)).ReturnsAsync(client);
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync((Restaurant)null!);
+
+            Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+            await act.Should().ThrowAsync<NotFoundException>();
+
+        }
+
+        [Fact()]
+        public async Task UpdateBipper_WithNonExistingClient_ReturnsNotFoundException()
+        {
+            var restaurantId = 1;
+            var restaurant = new Restaurant { Id = restaurantId };
+            var currentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+            var client = new Client { Id = 1 };
+            var bipper = new Bipper { RestaurantId = restaurantId };
+
+            var command = new UpdateBipperCommand { RestaurantId = restaurantId, ClientId = 1 };
+
+            _clientRepositoryMock.Setup(c => c.FindByIdAsync(1)).ReturnsAsync((Client)null!);
+
+            Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+            await act.Should().ThrowAsync<NotFoundException>();
+
+        }
+
+        [Fact()]
+        public async Task UpdateBipper_WithNonExistingBipper_ReturnsNotFoundException()
+        {
+            //arrange
+
+            var restaurantId = 1;
+            var restaurant = new Restaurant { Id = restaurantId };
+
+            var currentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+            var client = new Client { Id = 1 };
+
+            var bipperId = new Guid();
+            var bipper = new Bipper
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Pending,
+                IsReady = false,
+                IsActive = true,
+                Type = BipperType.TableAssignment
+            };
+
+            var command = new UpdateBipperCommand
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Notified,
+                IsReady = true,
+            };
+
+
+            _userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+            _clientRepositoryMock.Setup(c => c.FindByIdAsync(1)).ReturnsAsync(client);
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync(restaurant);
+
+            _bipperRepositoryMock.Setup(b => b.FindByIdAsync(bipperId)).ReturnsAsync((Bipper)null!);
+
+
+            //act
+
+            Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+            await act.Should().ThrowAsync<NotFoundException>();
+        }
+
+        [Fact()]
+        public async Task UpdateBipper_WithNotValidResource_ReturnsInvalidOperationException()
+        {
+
+            //arrange
+
+            var restaurantId = 1;
+            var restaurant = new Restaurant { Id = restaurantId };
+
+            var currentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+            var client = new Client { Id = 1 };
+
+            var bipperId = new Guid();
+            var bipper = new Bipper
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Pending,
+                IsReady = false,
+                IsActive = true,
+                Type = BipperType.TableAssignment
+            };
+
+            var command = new UpdateBipperCommand
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Notified,
+                IsReady = true,
+            };
+
+
+            _userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+            _clientRepositoryMock.Setup(c => c.FindByIdAsync(1)).ReturnsAsync(client);
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync(restaurant);
+
+            _bipperRepositoryMock.Setup(b => b.FindByIdAsync(bipperId)).ReturnsAsync(bipper);
+
+            _bipperAuthorizationServiceMock
+            .Setup(bas => bas.EnsureValidResource(restaurant, ResourceOperation.Update))
+            .Throws(new ForbidException());
+
+
+
+            //act
+
+            Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+
+            //assert
+
+            await act.Should().ThrowAsync<ForbidException>();
+        }
+
+        [Fact()]
+        public async Task UpdateBipper_BipperDontBelongsToRestaurant_ReturnsInvalidOperationException()
+        {
+
+            //arrange
+
+            var restaurantId = 1;
+            var restaurant = new Restaurant { Id = restaurantId };
+
+            var currentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+            var client = new Client { Id = 1 };
+
+            var bipperId = new Guid();
+            var bipper = new Bipper
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Pending,
+                IsReady = false,
+                IsActive = true,
+                Type = BipperType.TableAssignment
+            };
+
+            var command = new UpdateBipperCommand
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Notified,
+                IsReady = true,
+            };
+
+
+            _userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+            _clientRepositoryMock.Setup(c => c.FindByIdAsync(1)).ReturnsAsync(client);
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync(restaurant);
+
+            _bipperRepositoryMock.Setup(b => b.FindByIdAsync(bipperId)).ReturnsAsync(bipper);
+
+            _bipperAuthorizationServiceMock
+            .Setup(bas => bas.EnsureBipperBelongsToRestaurant(restaurant, bipper))
+            .Throws(new ForbidException());
+
+
+
+            //act
+
+            Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+
+            //assert
+
+            await act.Should().ThrowAsync<ForbidException>();
+        }
+
+        [Fact()]
+        public async Task UpdateBipper_BipperDontBelongsToClient_ReturnsInvalidOperationException()
+        {
+
+            //arrange
+
+            var restaurantId = 1;
+            var restaurant = new Restaurant { Id = restaurantId };
+
+            var currentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+            var client = new Client { Id = 1 };
+
+            var bipperId = new Guid();
+            var bipper = new Bipper
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Pending,
+                IsReady = false,
+                IsActive = true,
+                Type = BipperType.TableAssignment
+            };
+
+            var command = new UpdateBipperCommand
+            {
+                Id = bipperId,
+                RestaurantId = restaurantId,
+                ClientId = client.Id,
+                Status = BipperStatus.Notified,
+                IsReady = true,
+            };
+
+
+            _userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+            _clientRepositoryMock.Setup(c => c.FindByIdAsync(1)).ReturnsAsync(client);
+            _restaurantRepositoryMock.Setup(r => r.GetByIdAsync(restaurantId)).ReturnsAsync(restaurant);
+
+            _bipperRepositoryMock.Setup(b => b.FindByIdAsync(bipperId)).ReturnsAsync(bipper);
+
+            _bipperAuthorizationServiceMock
+            .Setup(bas => bas.EnsureBipperBelongsToClient(client, bipper))
+            .Throws(new InvalidOperationException());
+
+
+
+            //act
+
+            Func<Task> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+
+            //assert
+
+            await act.Should().ThrowAsync<InvalidOperationException>();
         }
     }
 }
---
+++ tests/Restaurants.ApplicationTests/Bippers/Commands/UpdateBipper/UpdateBipperCommandValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Restaurants.Application.Bippers.Commands.UpdateBipper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Bippers.Commands.UpdateBipper.Tests
+{
+    [TestClass()]
+    public class UpdateBipperCommandValidatorTests
+    {
+        [TestMethod()]
+        public void UpdateBipperCommandValidatorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
